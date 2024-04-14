7b8f93c6f7ca57bd1f496faca2dc4ff2b0349dc1
DANIEL ALBERTO DIAZ PEREZ <dadiazpe@correo.url.edu.gt>
2024-04-13 18:36:09 -0600

    3rd commit

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -6,17 +6,29 @@ import java.util.Scanner;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.jsonparsing.*;
 
+
+
 public class Main {
     public static void main(String[] args) throws IOException {
-        File file = new File("c:\\Users\\Dany\\Downloads\\input_lab_2_example(2).jsonl");
+        File auctions = new File("c:\\Users\\Dany\\Downloads\\input_auctions_example_lab_3(1).jsonl");
+        File customers = new File("c:\\Users\\Dany\\Downloads\\input_customer_example_lab_3(1).jsonl");
 
-        try (Scanner scan = new Scanner(file)) {
-            while(scan.hasNextLine()){
+        try (Scanner scanAunctions = new Scanner(auctions); Scanner scanCustomers = new Scanner(customers)) {
+            while(scanAunctions.hasNextLine()){
 
-                String line = scan.nextLine();
+                String auctionsLine = scanAunctions.nextLine();
+                String customersLine = scanCustomers.nextLine();
 
-                JsonNode node = json.parse(line);
                 
+
+                JsonNode auctionsNode = json.parse(auctionsLine);
+                JsonNode customersNode = json.parse(customersLine);
+
+
+                System.out.println(auctionsNode);
+                System.out.println(customersNode);
+
+
             }
         }
     }
